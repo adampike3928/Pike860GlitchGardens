25c967fce259411c731a74e2fa943cb27077e078
adampike334 <adampikespam@gmail.com>
2020-03-14 19:47:14 -0400

    Make a Splash Screen

---
+++ Assets/Scipts/LevelLoad.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+    
+{
+    [SerializeField] int timeToWait = 4;
+    
+    //looks for current scene
+    int currentSceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {//Obtains active scene
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {//Start Load time
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        //wait to load
+        LoadNextScene();
+
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

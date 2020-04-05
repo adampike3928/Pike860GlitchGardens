7cfe603859b06d89dc74f89521d9c3e4d6ae29c4
adampike334 <adampikespam@gmail.com>
2020-04-05 15:30:46 -0400

    Move Using transform.Transform

---
+++ Assets/Scipts/Attacker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range (0f, 5f)]
+    //Adds the walk speed varable and gives it a value
+    [SerializeField] float walkSpeed = 1f;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {//Equation for movment of sprite
+        transform.Translate(Vector2.left * walkSpeed *Time.deltaTime);
+        
+    }
+}

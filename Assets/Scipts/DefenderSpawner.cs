305367aea28376548fd9fabd7cc0fa414bdeb3a4
adampike334 <adampikespam@gmail.com>
2020-04-13 13:09:46 -0400

    Click to Spawn Defenders

---
+++ Assets/Scipts/DefenderSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+    private void OnMouseDown() 
+    
+    {
+        Debug.Log("mouse was clicked");
+    }
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity) as GameObject;
+    }
+
+
+}
+
+    
